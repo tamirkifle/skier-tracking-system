755699d5ecfdd4cc74dde93abc36c76acf6d11f8
Tamir Yirga <tkifle@gmail.com>
2026-09-12 13:10:48 -0700

    pin what the data model fixes

---
+++ Server/SkierServer/src/main/java/skiers/Constants.java
@@ -0,0 +1,18 @@
+package skiers;
+
+/** Values fixed by the data model, the API contract, or the broker topology. */
+public final class Constants {
+
+  private Constants() {}
+
+  public static final int MIN_RESORT_ID = 1;
+  public static final int MAX_RESORT_ID = 10;
+  public static final int SEASON_ID = 2025;
+  public static final int DAY_ID = 1;
+  public static final int MIN_SKIER_ID = 1;
+  public static final int MAX_SKIER_ID = 100000;
+  public static final int MIN_LIFT_ID = 1;
+  public static final int MAX_LIFT_ID = 40;
+  public static final int MIN_TIME = 1;
+  public static final int MAX_TIME = 360;
+}
