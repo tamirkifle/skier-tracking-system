1d5e5f3e2a09ba4ddd2b7873b1b87ae70a601590
Tamir Yirga <tkifle@gmail.com>
2026-09-12 13:10:35 -0700

    add the server module

---
+++ Server/SkierServer/src/main/java/skiers/SkierServerApplication.java
@@ -0,0 +1,15 @@
+package skiers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@ConfigurationPropertiesScan
+public class SkierServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SkierServerApplication.class, args);
+  }
+}
