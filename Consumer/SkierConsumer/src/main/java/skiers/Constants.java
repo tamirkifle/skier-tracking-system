5de5adb63ca00d236882bb0e6f04060ed1309fce
Tamir Yirga <tkifle@gmail.com>
2026-09-12 17:53:18 -0700

    project an event into an item

--- Consumer/SkierConsumer/src/main/java/skiers/Constants.java
+++ Consumer/SkierConsumer/src/main/java/skiers/Constants.java
@@ -11,6 +11,22 @@ public final class Constants {
   public static final String LIFT_RIDE_ROUTING_KEY = "lift.ride";
   public static final String DEAD_LETTER_ROUTING_KEY = "dead.letter";
 
+  public static final String LIFT_RIDES_TABLE = "LiftRides";
+
+  public static final String ATTR_SKIER_ID = "skierID";
+  public static final String ATTR_SORT_KEY = "resortID#seasonID#dayID#timestamp";
+  public static final String ATTR_SKIER_SEASON = "skierID#seasonID";
+  public static final String ATTR_RESORT_DAY = "resortID#dayID";
+  public static final String ATTR_RESORT_SKIER = "resortID#skierID";
+  public static final String ATTR_SEASON_DAY = "seasonID#dayID";
+  public static final String ATTR_RESORT_SEASON_DAY = "resortSeasonDay";
+  public static final String ATTR_RESORT_ID = "resortID";
+  public static final String ATTR_SEASON_ID = "seasonID";
+  public static final String ATTR_DAY_ID = "dayID";
+  public static final String ATTR_LIFT_ID = "liftID";
+  public static final String ATTR_TIMESTAMP = "timestamp";
+  public static final String ATTR_VERTICAL = "vertical";
+
   public static final String HEADER_EVENT_ID = "x-event-id";
 
   public static final String HEADER_PUBLISHED_AT = "x-published-at";
---
+++ Consumer/SkierConsumer/src/main/java/skiers/persistence/LiftRideItems.java
@@ -0,0 +1,36 @@
+package skiers.persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+import skiers.Constants;
+import skiers.model.LiftRideEvent;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+/** The single projection of an event into a LiftRides item, materialised GSI keys included. */
+public final class LiftRideItems {
+
+  private LiftRideItems() {}
+
+  /** liftID, timestamp and vertical are strings; a number stored as a string sorts lexically. */
+  public static Map<String, AttributeValue> toAttributeMap(LiftRideEvent event) {
+    Map<String, AttributeValue> item = new HashMap<>(16);
+    item.put(Constants.ATTR_SKIER_ID, str(event.skierId()));
+    item.put(Constants.ATTR_SORT_KEY, str(event.sortKey()));
+    item.put(Constants.ATTR_RESORT_ID, str(event.resortId()));
+    item.put(Constants.ATTR_SEASON_ID, str(event.seasonId()));
+    item.put(Constants.ATTR_DAY_ID, str(event.dayId()));
+    item.put(Constants.ATTR_LIFT_ID, str(String.valueOf(event.liftId())));
+    item.put(Constants.ATTR_TIMESTAMP, str(String.valueOf(event.time())));
+    item.put(Constants.ATTR_VERTICAL, str(String.valueOf(event.vertical())));
+    item.put(Constants.ATTR_SKIER_SEASON, str(event.skierSeasonKey()));
+    item.put(Constants.ATTR_RESORT_DAY, str(event.resortDayKey()));
+    item.put(Constants.ATTR_RESORT_SKIER, str(event.resortSkierKey()));
+    item.put(Constants.ATTR_SEASON_DAY, str(event.seasonDayKey()));
+    item.put(Constants.ATTR_RESORT_SEASON_DAY, str(event.resortSeasonDayKey()));
+    return item;
+  }
+
+  private static AttributeValue str(String value) {
+    return AttributeValue.builder().s(value).build();
+  }
+}
