f9e16f9a9f1d3ad39bf31c77e4b30118590871de
Tamir Yirga <tkifle@gmail.com>
2026-09-12 17:51:48 -0700

    pin the names the consumer shares with the server

---
+++ Consumer/SkierConsumer/src/main/java/skiers/Constants.java
@@ -0,0 +1,17 @@
+package skiers;
+
+public final class Constants {
+
+  private Constants() {}
+
+  public static final String MAIN_QUEUE = "liftRideQueue";
+  public static final String DLX = "deadLetterExchange";
+  public static final String DLQ = "deadLetterQueue";
+  public static final String LIFT_RIDE_EXCHANGE = "liftRideExchange";
+  public static final String LIFT_RIDE_ROUTING_KEY = "lift.ride";
+  public static final String DEAD_LETTER_ROUTING_KEY = "dead.letter";
+
+  public static final String HEADER_EVENT_ID = "x-event-id";
+
+  public static final String HEADER_PUBLISHED_AT = "x-published-at";
+}
