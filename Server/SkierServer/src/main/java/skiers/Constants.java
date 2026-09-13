74377a7d714ccfc40678e37041e16054e2773156
Tamir Yirga <tkifle@gmail.com>
2026-09-12 17:47:25 -0700

    name the broker topology

--- Server/SkierServer/src/main/java/skiers/Constants.java
+++ Server/SkierServer/src/main/java/skiers/Constants.java
@@ -5,6 +5,18 @@ public final class Constants {
 
   private Constants() {}
 
+  public static final String MAIN_QUEUE = "liftRideQueue";
+  public static final String LIFT_RIDE_EXCHANGE = "liftRideExchange";
+  public static final String LIFT_RIDE_ROUTING_KEY = "lift.ride";
+  public static final String DLX = "deadLetterExchange";
+  public static final String DLQ = "deadLetterQueue";
+  public static final String DEAD_LETTER_ROUTING_KEY = "dead.letter";
+
+  public static final String HEADER_EVENT_ID = "x-event-id";
+
+  /** Epoch millis handed to the broker; the AMQP timestamp property is whole seconds. */
+  public static final String HEADER_PUBLISHED_AT = "x-published-at";
+
   public static final int MIN_RESORT_ID = 1;
   public static final int MAX_RESORT_ID = 10;
   public static final int SEASON_ID = 2025;
